f66c28f1ec500fa228ad5782449a233970e33322
Sharavana <sharavana.kumar.0987@gmail.com>
2024-11-12 14:32:40 +0530

    added comment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-  console.log(`Client connected: ${socket.id}`);
+  console.log(`Client connected: ${socket.id}`);//some random hash
 
 
   // Relay file metadata to other clients
